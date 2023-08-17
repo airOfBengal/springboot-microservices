b21d88d6f29c070de91601136194fddc59393b29
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2023-08-17 12:55:22 +0600

    Add microservice communication using WebClient

--- employee-service/src/main/java/com/example/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/example/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -12,13 +12,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository repository;
     private ModelMapper modelMapper;
-    private RestTemplate restTemplate;
+    //private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -36,9 +38,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         );
 
         EmployeeDto employeeDto = modelMapper.map(employee, EmployeeDto.class);
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
-                "http://localhost:8090/api/departments/"+ employeeDto.getDepartmentCode(), DepartmentDto.class);
-        DepartmentDto departmentDto = responseEntity.getBody();
+
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
+//                "http://localhost:8090/api/departments/"+ employeeDto.getDepartmentCode(), DepartmentDto.class);
+//        DepartmentDto departmentDto = responseEntity.getBody();
+
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8090/api/departments/"+ employeeDto.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         ApiResponseDto apiResponseDto = new ApiResponseDto();
         apiResponseDto.setEmployee(employeeDto);
